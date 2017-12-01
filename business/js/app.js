a46feac174bb6a5cefb988cd166881da0b814a24
Vitaliy Yakovenko <pheonix.khmao@gmail.com>
2017-12-02 03:48:57 +0500

    tmp

--- business/js/app.js
+++ business/js/app.js
@@ -32,51 +32,8 @@ app.onPageInit('business-2', () => {
 })
 
 app.onPageInit('business-11', () => {
-  // Conversation flag
-  const conversationStarted = false;
-
-// Init Messages
-  const myMessages = app.messages('.messages', {
-    autoLayout:true
-  });
-
-// Init Messagebar
-  const myMessagebar = app.messagebar('.messagebar');
-
-// Handle message
-  $('.messagebar .link').on('click', function () {
-    // Message text
-    const messageText = myMessagebar.value().trim();
-    // Exit if empy message
-    if (messageText.length === 0) return;
-
-    // Empty messagebar
-    myMessagebar.clear()
-
-    // Random message type
-    const messageType = (['sent', 'received'])[Math.round(Math.random())];
-
-    // Avatar and name for received message
-    let avatar, name;
-    if(messageType === 'received') {
-      avatar = 'http://lorempixel.com/output/people-q-c-100-100-9.jpg';
-      name = 'Kate';
-    }
-    // Add message
-    myMessages.addMessage({
-      // Message text
-      text: messageText,
-      // Random message type
-      type: messageType,
-      // Avatar and name:
-      avatar: avatar,
-      name: name,
-      // Day
-      day: !conversationStarted ? 'Today' : false,
-      time: !conversationStarted ? (new Date()).getHours() + ':' + (new Date()).getMinutes() : false
-    })
-
-    // Update conversation flag
-    conversationStarted = true;
-  });
+  app.messages('.messages', {
+    autoLayout: true
+  })
+  app.messagebar('.messagebar')
 })
