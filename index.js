c20dd13e223d5db37f1e714d775e9446bc249e4d
bpa <bpa@ugraweb.ru>
2017-12-02 01:28:05 +0500

    tmp

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const bs = require('browser-sync').create()
 
-bs.watch('./public').on('change', bs.reload)
+bs.watch('./customer').on('change', bs.reload)
 bs.init({
-  server: './public'
+  server: './customer'
 })
