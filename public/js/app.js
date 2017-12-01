9789aab1d8fb8c12f5b54b9b3e13bfe528c6277f
Vitaliy Yakovenko <pheonix.khmao@gmail.com>
2017-12-02 01:24:09 +0500

    tmp

--- public/js/app.js
+++ public/js/app.js
@@ -7,3 +7,10 @@ const view = app.addView('.view-main', {})
 
 $(document).on('ajaxStart', () => app.showIndicator())
 $(document).on('ajaxComplete', () => app.hideIndicator())
+
+addEventListener('message', (e) => {
+  const { data } = e
+  console.log(data)
+
+  view.router.loadPage('about.html')
+}, false)
